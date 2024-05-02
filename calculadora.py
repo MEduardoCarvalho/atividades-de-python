b7331b595b62bce1f3e7abbc91cedaf37273e78d
marcos eduardo <165714407+MEduardoCarvalho@users.noreply.github.com>
2024-05-02 06:50:26 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,13 @@
+termo = int(input("digite o primeiro termo "))
+razao = int(input("digitea razao "))
+c=0
+fim = 10
+print(f"{termo} ", end = "")
+while c < fim:
+  termo += razao
+  print(f"{termo} " , end = "")
+  c+=1
+  if c == fim:
+    num = int(input(" \n quer mais quantos termos "))
+    fim += num
+print(f"foram mostrado {fim} termos")
